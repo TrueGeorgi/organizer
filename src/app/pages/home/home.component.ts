007230966e6f23d8dfe526c11137bb8210d8977d
TrueGeorgi <110535908+TrueGeorgi@users.noreply.github.com>
2025-10-17 12:03:04 +0200

    card-component created and added to the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { CardComponent } from "../../shared/reusables/card/card.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [MatIconModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatGridListModule],
+  imports: [MatIconModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatGridListModule, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
